128ad14c6d8d55d699eda7c0665ea34cb90a6ff8
IT21003332 <it21003332@my.sliit.lk>
2023-03-10 11:57:16 +0530

    +carview Implementation

--- client/src/Pages/Mainfile.jsx
+++ client/src/Pages/Mainfile.jsx
@@ -13,19 +13,41 @@ const Mainfile = () =>{
     const searchMovies = async(title) =>{
         const response  = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
-
+        
         setMovies(data.Search);
+        console.log(data.Search)
     }
 
     //Reserved space for hooks
     const [movies, setMovies] = useState([])
     const [searchTerm,setSearchTerm] = useState("");
     useEffect(()=>{
-       // searchMovies('Spiderman');
+        //searchMovies('Spiderman');
     },[])
 
     return (
-        <div className="flex relative dark: bg-main-dark-bg">
+        <div className="relative dark:bg-main-dark-bg dark:text-white min-h-screen">
+            <div className="p-1 flex w-full h-20 rounded px-10">
+                <div className={`pl-2 text-white
+                transition ease-in-out delay-150  
+                hover:translate-y-1 hover:scale-110 hover:text-cyan-200 duration-500`}>
+                    <h1 className="text-2xl pt-2 pl-2"> MovieCentra</h1>
+                </div> 
+                <div className="flex px-20 pt-3 pb-3
+                transition ease-in-out delay-150 hover:translate-y-1 hover:scale-105 hover:placeholder:text-gray-800 duration-500">
+                    <input className=" text-center h-10 w-800 text-white 
+                    rounded-md bg-slate-500 placeholder:text-gray-400 outline-none" 
+                    placeholder="Search Movies"
+                    onChange={(e)=>{
+                        setSearchTerm(e.target.value);
+                    }} onSubmit={()=> searchMovies(searchTerm)}/>
+                    <button className="flex "onClick={()=>searchMovies(searchTerm)}>
+                    <img className=" w-10 h-10 pl-1 " src={find}
+                        alt="search"></img>
+                    </button>
+                </div>
+            </div>
+
             {/* <h1>MovieCentra</h1> */}
             {/* <div className="search">
                 <input
@@ -38,20 +60,19 @@ const Mainfile = () =>{
                 alt="search"
                 onClick={()=>searchMovies(searchTerm)}/>
             </div> */}
-            {/* {movies?.length > 0 ? (<div className="container">
+             {movies.length > 0 ? (<div className="flex flex-wrap p-5">
                 {movies.map((movie,key)=>{
                     return(
-                        <div key={key}>
+                        <div key={key} className="p-5">
                             <MovieCard movie={movie}/>
                         </div>
-                        
                     )
                 })}
             </div>) : (
                 <div className="empty">
                     <h2>No Movies Found!</h2>
                 </div>
-            )} */}
+            )} 
 
         </div>
     )
